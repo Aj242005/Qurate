af7bf933119ae68ddc9fcade17a9a0761596962e
Aj242005 <akshitjain6661@gmail.com>
2026-02-28 02:04:16 +0530

    added user model and error and success response model and few other minor tweaks

---
+++ backend-py/models/falseRes.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel,Field
+from typing import Annotated
+
+class ErrRes(BaseModel):
+    status =  Annotated(int ,Field(default=200,description="This is the HTTP status code. This is used to provide an early categorization of response"))
+    message = Annotated(str, Field(description="This is the description of the error which have occured"))
--- backend-py/models/trueRes.py
+++ backend-py/models/trueRes.py
@@ -1,1 +1,18 @@
-from pydantic 
+from pydantic import BaseModel,Field
+from typing import Annotated
+
+class SuccessRes(BaseModel):
+    status =  Annotated(int ,Field(default=200,description="This is the HTTP status code. This is used to provide an early categorization of response"))
+    message = Annotated(str, Field(description="This is the description which is a required thing for a successful response"))
+
+
+
+
+
+def something(param)->SuccessRes | ErrRes :
+
+
+    return {
+        "status" : 200,
+        "message" : "everything is working fine"
+    }
