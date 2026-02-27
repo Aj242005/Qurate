5a68efd4cafa67428987eb1621a152a3cbeefc79
Aj242005 <akshitjain6661@gmail.com>
2026-02-28 02:06:57 +0530

    just a mistake --reverting

--- backend-py/models/trueRes.py
+++ backend-py/models/trueRes.py
@@ -5,14 +5,3 @@ class SuccessRes(BaseModel):
     status =  Annotated(int ,Field(default=200,description="This is the HTTP status code. This is used to provide an early categorization of response"))
     message = Annotated(str, Field(description="This is the description which is a required thing for a successful response"))
 
-
-
-
-
-def something(param)->SuccessRes | ErrRes :
-
-
-    return {
-        "status" : 200,
-        "message" : "everything is working fine"
-    }
