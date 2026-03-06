832e6194181aa1e929ecc42d144b463cb9bd36d9
Aj242005 <akshitjain6661@gmail.com>
2026-03-07 02:17:07 +0530

    nothing just a new function on redis

--- backend-py/redis/redis_main.py
+++ backend-py/redis/redis_main.py
@@ -1,7 +1,20 @@
 import redis
-from models import userModel
+from models import userModel,falseRes,trueRes
 class RedisBasic:
     def __init__(self,host = "localhost"):
         self.redisClient = redis.Redis(host=host,decode_responses=True)
     
-    def addUserDataToRedis(self,user : userModel.User):
+    def addUserDataToRedis(self,user : userModel.User)-> falseRes.ErrRes | trueRes.SuccessRes :
+        try:
+            response = self.redisClient.hset(f'user:{user.user_id}',mapping=user)
+            return trueRes.SuccessRes(
+                status = 201,
+                message = "successfully added the user to redis",
+                anotherValid = response
+            )
+        except Exception as e:
+            return falseRes.ErrRes(
+                status = 511,
+                message = "Error adding user data to the redis Client",
+                anotherValid = e 
+            )
