5619628f2cd0be43835cd2f8c7d54a02c2e9873d
Aj242005 <akshitjain6661@gmail.com>
2026-03-08 01:34:56 +0530

    added a function to retrieve user data from redis for high speed and low bandwidth data access

--- backend-py/redis/redis_main.py
+++ backend-py/redis/redis_main.py
@@ -3,7 +3,7 @@ from models import userModel,falseRes,trueRes
 class RedisBasic:
     def __init__(self,host = "localhost"):
         self.redisClient = redis.Redis(host=host,decode_responses=True)
-    
+
     def addUserDataToRedis(self,user : userModel.User)-> falseRes.ErrRes | trueRes.SuccessRes :
         try:
             response = self.redisClient.hset(f'user:{user.user_id}',mapping=user)
@@ -16,5 +16,19 @@ class RedisBasic:
             return falseRes.ErrRes(
                 status = 511,
                 message = "Error adding user data to the redis Client",
+                anotherValid = e 
+            )
+    def getUserDetailsFromRedis(self, user_id : str) -> falseRes.ErrRes | trueRes.SuccessRes :
+        try:
+            response = self.redisClient.hgetall(f'user:{user_id}')
+            return trueRes.SuccessRes(
+                status=200,
+                message="successfully retrieved the user data from redis",
+                anotherValid=response,
+            )
+        except Exception as e:
+            return falseRes.ErrRes(
+                status = 512,
+                message = "Error retrieving user data from the redis Client",
                 anotherValid = e 
             )
