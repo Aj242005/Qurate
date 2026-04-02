a80afc39fb6f82edbdf383e615d4a9324c42ba5d
Aj242005 <akshitjain6661@gmail.com>
2026-04-03 01:09:48 +0530

    Completed authentication and authorization

---
+++ backend-py/redisdb/redis_main.py
@@ -0,0 +1,85 @@
+import redis
+from models import userModel, falseRes, trueRes
+
+class RedisBasic:
+    def __init__(self, host="localhost"):
+        self.redisClient = redis.Redis(host=host, decode_responses=True)
+    
+    def addRefreshTokenToRedis(self, refreshToken: str, email: str) -> falseRes.ErrRes | trueRes.SuccessRes:
+        try:
+            response = self.redisClient.hset(f'refresh:{email}', mapping={refreshToken: "True"})
+            self.redisClient.expire(f'refresh:{email}', 604800)
+            return trueRes.SuccessRes(
+                status=201,
+                message="New refresh token is added to the redis client",
+                anotherValid=response
+            )
+        except Exception as e:
+            return falseRes.ErrRes(
+                status=511,
+                message="Error adding refresh token to the redis client",
+                anotherValid=e
+            )
+
+    def invalidateRefreshToken(self, refreshToken: str, email: str) -> falseRes.ErrRes | trueRes.SuccessRes:
+        try:
+            response = self.redisClient.hset(f'refresh:{email}', mapping={refreshToken: "False"})
+            self.redisClient.expire(f'refresh:{email}', 604800)
+            return trueRes.SuccessRes(
+                status=201,
+                message="Invalidated a refresh token",
+                anotherValid=response
+            )
+        except Exception as e:
+            return falseRes.ErrRes(
+                status=511,
+                message="Error invalidating refresh token",
+                anotherValid=e
+            )
+
+    def checkRefreshTokenStatus(self, refreshToken: str, email: str) -> falseRes.ErrRes | trueRes.SuccessRes:
+        try:
+            response = self.redisClient.hget(f'refresh:{email}', refreshToken)
+            return trueRes.SuccessRes(
+                status=200,
+                message="Status of refresh token's validity",
+                anotherValid=True if response in ("True", "true", "1") else False
+            )
+        except Exception as e:
+            return falseRes.ErrRes(
+                status=511,
+                message="Error checking refresh token's status",
+                anotherValid=e
+            )
+
+    def addUserDataToRedis(self, user: userModel.User) -> falseRes.ErrRes | trueRes.SuccessRes:
+        try:
+            safe_mapping = {k: str(v) for k, v in user.model_dump().items() if v is not None}
+            response = self.redisClient.hset(f'user:{user.email}', mapping=safe_mapping)
+            self.redisClient.expire(f'user:{user.email}', time=1800)
+            return trueRes.SuccessRes(
+                status=201,
+                message="successfully added the user to redis",
+                anotherValid=response
+            )
+        except Exception as e:
+            return falseRes.ErrRes(
+                status=511,
+                message="Error adding user data to the redis Client",
+                anotherValid=e 
+            )
+
+    def getUserDetailsFromRedis(self, email: str) -> falseRes.ErrRes | trueRes.SuccessRes:
+        try:
+            response = self.redisClient.hgetall(f'user:{email}')
+            return trueRes.SuccessRes(
+                status=200,
+                message="successfully retrieved the user data from redis",
+                anotherValid=response,
+            )
+        except Exception as e:
+            return falseRes.ErrRes(
+                status=512,
+                message="Error retrieving user data from the redis Client",
+                anotherValid=e 
+            )
--- backend-py/src/main.py
+++ backend-py/src/main.py
@@ -1,25 +1,25 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Request
 from dotenv import load_dotenv
 import os
-from models import falseRes, userModel, trueRes
+from models import falseRes, userModel, trueRes, payload
 from utils import generateuuid,password
 from db import mongo
 from utils.jwt import JWToken
-
+from redisdb.redis_main import RedisBasic
 load_dotenv()
 
 mongo_uri = str(os.getenv("MONGO_URI"))
 gemini_api = str(os.getenv("GEMINI_API_KEY"))
 access_secret = str(os.getenv("ACCESS_TOKEN_SECRET_KEY"))
 access_duration = int(os.getenv("ACCESS_TOKEN_DURATION", "60"))
-refresh_secret = str(os.getenv("REFRESH_TOKNE_SECRET_KEY"))
+refresh_secret = str(os.getenv("REFRESH_TOKEN_SECRET_KEY"))
 refresh_duration = int(os.getenv("REFRESH_TOKEN_DURATION", "30"))
 server = FastAPI()
 
 mongo_db = mongo.MongoDB(mongo_uri)
 access_token = JWToken(secret=access_secret,duration=access_duration,typeof="access")
 refresh_token = JWToken(secret=refresh_secret,duration=refresh_duration,typeof="refresh")
-
+redis_client = RedisBasic()
 @server.get("/")
 def homeRoute():
     return {
@@ -45,8 +45,8 @@ async def signUp(user: userModel.UserRes):
             "status" : 409,
             "message":"User with same email or number exists",
             "anotherValid" : {
-                                user.email,
-                                user.phone_number
+                                "email" : user.email,
+                                "phone_number" :user.phone_number
                             }
         }
     else:
@@ -57,7 +57,7 @@ async def signUp(user: userModel.UserRes):
     
         
 @server.post('/login', response_model= falseRes.ErrRes | trueRes.SuccessRes)
-async def login(user: userModel.loginReq):
+async def login(user: userModel.loginReq, response : Response, request : Request):
     db_user = mongo_db.retreieveUserInfo(email=user.email)
     if db_user is None:
         return falseRes.ErrRes(
@@ -81,18 +81,72 @@ async def login(user: userModel.loginReq):
         email = db_user["email"]
         user_id = db_user["user_id"]
         name = db_user["name"]
-        payload = {
+        payloadd = {
             "email" : email ,
             "user_id": user_id,
             "name": name
         }
-
+        payload_instance = payload.Payload.model_validate(payloadd)
+        response.headers["accessToken"] = str(access_token.createToken(payload_instance).anotherValid)
+        rfToken = str(refresh_token.createToken(payload_instance).anotherValid)
+        if ( request.headers.get("refreshToken") is not None ):
+            redis_client.invalidateRefreshToken(refreshToken=str(request.headers.get("refreshToken")),email=email)
+        response.headers["refreshToken"] = rfToken
+        redis_client.addRefreshTokenToRedis(rfToken,email)
         return trueRes.SuccessRes(
             status=200,
             message="logged in succesfully",
             anotherValid = None
         )
-    
+
+
+
+@server.post('/refresh')
+async def refreshTokenGeneration( response : Response , request : Request )-> falseRes.ErrRes | trueRes.SuccessRes:
+    rfToken = request.headers.get("refreshToken")
+    if(rfToken is not None):
+        verificationStatus = refresh_token.verifyToken(rfToken)
+        if(verificationStatus.status is 200):
+            if verificationStatus.anotherValid is None:
+                return falseRes.ErrRes(
+                    status = 401,
+                    message = "Invalid token payload",
+                    anotherValid = None
+                )
+            userEmail = str(verificationStatus.anotherValid.email)
+            refreshTokenStatus = redis_client.checkRefreshTokenStatus(refreshToken=rfToken,email=userEmail)
+            if(refreshTokenStatus.anotherValid):
+                redis_client.invalidateRefreshToken(rfToken,userEmail)
+                payloadd = verificationStatus.anotherValid
+                newRf = refresh_token.createToken(payloadd).anotherValid
+                response.headers["acccessToken"] = str(access_token.createToken(payloadd).anotherValid)
+                response.headers["refreshToken"] = str(newRf)
+                redis_client.addRefreshTokenToRedis(str(newRf),userEmail)
+                return trueRes.SuccessRes(
+                    status = 200,
+                    message = "Valid refresh token used",
+                    anotherValid = None
+                )
+            else:
+                return falseRes.ErrRes(
+                    status = 401,
+                    message = "Invalid Refresh token used",
+                    anotherValid = None
+                )
+        else:
+            return falseRes.ErrRes(
+                status = 401,
+                message = "Tampered or expired refresh token is provided by the client",
+                anotherValid = verificationStatus.anotherValid
+            )
+    else:
+        return falseRes.ErrRes(
+            status = 404,
+            message = "Refresh Token not found in the headers",
+            anotherValid = None
+        )
+
+
 @server.get('/random-unit-testing-route')
 def unit_testing():
     return {
